e3da9d2c37e51878917359e6c19e200e4cf7171e
RSF <fink.spencer@gmail.com>
2018-07-20 11:49:00 -0400

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create({username: 'Tester', email: 'Test@Testing.com', password: 'password'})
+User.destroy_all
+Server.destroy_all
+Channel.destroy_all
+Message.destroy_all
+ServerMembership.destroy_all
+
+u1 = User.create({username: 'Tester', email: 'Test@Testing.com', password: 'password'})
+u2 = User.create({username: 'DangerousDave', email: 'DangerousDave', password: 'password'})
+u3 = User.create({username: 'SimpleSimon', email: 'SimpleSimon', password: 'password'})
+u4 = User.create({username: 'RanchDubois', email: 'RanchDubois', password: 'password'})
+u5 = User.create({username: 'BertMacklin', email: 'BertMacklin', password: 'password'})
+
+s1 = Server.create({server_owner_id: u1.id, server_name: 'Swerver'})
+s2 = Server.create({server_owner_id: u1.id, server_name: 'Cool Server'})
+s3 = Server.create({server_owner_id: u2.id, server_name: 'Staines Massive'})
+s4 = Server.create({server_owner_id: u3.id, server_name: 'This is a Server'})
+s5 = Server.create({server_owner_id: u4.id, server_name: 'Ranch Revolution'})
+s6 = Server.create({server_owner_id: u5.id, server_name: 'Mouserat'})
+
+sm1 = ServerMembership.create({server_id: s1.id, user_id: u1.id})
+sm2 = ServerMembership.create({server_id: s1.id, user_id: u2.id})
+
+sm3 = ServerMembership.create({server_id: s2.id, user_id: u1.id})
+sm4 = ServerMembership.create({server_id: s2.id, user_id: u2.id})
+sm5 = ServerMembership.create({server_id: s2.id, user_id: u3.id})
+sm6 = ServerMembership.create({server_id: s2.id, user_id: u4.id})
+sm7 = ServerMembership.create({server_id: s2.id, user_id: u5.id})
+
+sm8 = ServerMembership.create({server_id: s3.id, user_id: u2.id})
+sm9 = ServerMembership.create({server_id: s3.id, user_id: u3.id})
+
+sm10 = ServerMembership.create({server_id: s4.id, user_id: u3.id})
+sm11 = ServerMembership.create({server_id: s4.id, user_id: u5.id})
+
+sm12 = ServerMembership.create({server_id: s5.id, user_id: u4.id})
+sm13 = ServerMembership.create({server_id: s5.id, user_id: u5.id})
+
+sm14 = ServerMembership.create({server_id: s6.id, user_id: u5.id})
+sm15 = ServerMembership.create({server_id: s6.id, user_id: u2.id})
+
+
+c1 = Channel.create({channel_name: "Cool Channel", server_id: s2.id})
+c2 = Channel.create({channel_name: "Cooler Channel", server_id: s2.id})
+c3 = Channel.create({channel_name: "Coolest Channel", server_id: s2.id})
+
+c4 = Channel.create({channel_name: "This is a Channel", server_id: s4.id})
+
+c5 = Channel.create({channel_name: "Centurion Club: Elite VIP Area(Sponsored by SoBe Lifewater)", server_id: s6.id})
+c6 = Channel.create({channel_name: "The Department of Homeland Obscurity", server_id: s6.id})
