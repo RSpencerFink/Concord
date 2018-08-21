59c229b53323202d67b2fbfb8204fe96dbe3caa4
RSpencerFink <fink.spencer@gmail.com>
2018-08-21 16:33:05 -0400

    removes hardcoding

--- frontend/concord.jsx
+++ frontend/concord.jsx
@@ -31,14 +31,12 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   const CableApp = {};
-  let url = "wss://concord-chat.herokuapp.com/cable"
-  console.log(process.env.NODE_ENV === 'production');
-  console.log(process.env.NODE_ENV);
-  // if (process.env.NODE_ENV === 'production') {
-  //   url = `wss://concord-chat.herokuapp.com/cable`
-  // } else {
-  //   url = `ws://${window.location.hostname}:3000/cable`
-  // }
+  let url = ""
+  if (process.env.NODE_ENV === 'production') {
+    url = `wss://concord-chat.herokuapp.com/cable`
+  } else {
+    url = `ws://${window.location.hostname}:3000/cable`
+  }
 
   CableApp.cable = actionCable.createConsumer(url)
 
